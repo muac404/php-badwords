eb22dfb431f1f47162f9f67accab6169710f4e87
muac404 <muac404@gmail.com>
2022-12-13 16:38:09 +0100

    Aggiunge paragrafo e lunghezza paragrafo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 $badWords = $_GET=["ipsum"];
-$paragrafo = ["Lorem ipsum dolor sit amet consectetur adipisicing elit"];
+$paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit lorem ipsum dolor sit amet consectetur adipisicing elit";
 // var_dump($badWords);
 ?>
 <!DOCTYPE html>
@@ -12,6 +12,7 @@ $paragrafo = ["Lorem ipsum dolor sit amet consectetur adipisicing elit"];
     <title>PHP Badwords</title>
   </head>
   <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus expedita nobis cumque aut voluptas illum, molestiae dolore alias quos eum consequuntur enim fuga similique sint necessitatibus corporis, pariatur velit minima?</p>
+    <p><?php echo $paragrafo; ?></p>
+    <h3>Lunghezza del paragrafo: <?php echo strlen($paragrafo)?> caratteri </h3>
   </body>
 </html>
