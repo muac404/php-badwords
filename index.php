fb353e3246d8dd4c1e686509c1b2b3b34c3362ab
muac404 <muac404@gmail.com>
2022-12-13 16:32:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+$badWords = $_GET=["ipsum"];
+$paragrafo = ["Lorem ipsum dolor sit amet consectetur adipisicing elit"];
+// var_dump($badWords);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Badwords</title>
+  </head>
+  <body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus expedita nobis cumque aut voluptas illum, molestiae dolore alias quos eum consequuntur enim fuga similique sint necessitatibus corporis, pariatur velit minima?</p>
+  </body>
+</html>
